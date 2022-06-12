73de0c64abf2e0be9b50aa81aabd3844b2f79843
Erica Wanja <wanjaerica@gmail.com>
2022-06-12 20:25:51 +0300

    add bookmarks, play and delete functions

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,6 @@
 (()=>{
     let youtubeLeftControls,youtubePlayer;
-    let currentVideo= ""
+    let currentVideo= "";
     let currentVideoBookmarks= []
 
     chrome.runtime.onMessage.addListener((obj, sender, response)=>{
@@ -9,8 +9,16 @@
             currentVideo = videId;
             newVideoLoaded();
 
+        }else if(type === "PLAY"){
+            youtubePlayer.currentTime= value;
+        }else if( type === "DELETE"){
+            currentVideoBookmarks = currentVideoBookmarks.filter((b)=> b.time != value);
+            chrome.storage.sync.set({[currentVideo]: JSON.stringify(currentVideoBookmarks) });
+
+            response(currentVideoBookmarks);
         }
     });
+
     const fetchBookmarks =()=>{
         return new Promise((resolve)=>{
             chrome.storage.async.get([currentVideo], (obj)=>{
@@ -18,7 +26,8 @@
             })
         })
     }
-    const newVideoLoaded = ()=>{
+
+    const newVideoLoaded = async ()=>{
         const bookmarkBtnExists= document.getElementsByClassName("bookmark-btn")[0];
         currentVideoBookmarks = await fetchBookmarks();
         if(!bookmarkBtnExists){
@@ -35,7 +44,8 @@
             bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
         }
     }
-    const addNewBookmarkEventHandler = ()=>{
+
+    const addNewBookmarkEventHandler = async ()=>{
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time:currentTime,
@@ -46,7 +56,13 @@
             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a,b)=>a.time - b.time))
         })
     }
-    newVideoLoaded(); 
+    
+    let trail = "&ytExt=ON";
+    if(!window.location.href.includes(trail)&& !window.location.href.includes("ab_channel")){
+        if(window.location.href.includes("youtube.com/watch")){
+           window.location.href+=trail;
+        }
+    } 
 })();
 
 const getTime = t =>{
